2a2b69100e776943258dd8234d6113635f1a3de0
Alexandre Coene <a_coene@orange.fr>
2025-01-29 10:49:39 +0100

    Ajoutez des fichiers projet.

---
+++ Solary_Gestionnaire/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Solary_Gestionnaire
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Solary_Gestionnaire/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Solary_Gestionnaire.View;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Solary_Gestionnaire
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Accueil_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid != null)
+            {
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(new AccueilPage());
+            }
+        }
+
+        private void GestionnaireBornes_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid != null)
+            {
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(new GestionnaireBornePage());
+            }
+        }
+
+        private void GestionnaireUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid != null)
+            {
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(new GestionnaireUserPage());
+            }
+        }
+
+        private void Statistique_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid != null)
+            {
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(new StatistiquesPage());
+            }
+        }
+
+        private void PanneMaintenance_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid != null)
+            {
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(new PannePage());
+            }
+        }
+    }
+}
