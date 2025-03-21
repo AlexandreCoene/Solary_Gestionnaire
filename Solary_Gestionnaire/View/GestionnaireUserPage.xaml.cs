20d03eb18141d5b509166abff5057467408d5814
Alexandre Coene <a_coene@orange.fr>
2025-03-21 05:18:01 +0100

    Design USER avec API

--- Solary_Gestionnaire/View/GestionnaireUserPage.xaml.cs
+++ Solary_Gestionnaire/View/GestionnaireUserPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Solary_Gestionnaire.Model;
 using Solary_Gestionnaire.ViewModel;
 
 namespace Solary_Gestionnaire.View
@@ -12,6 +14,34 @@ namespace Solary_Gestionnaire.View
             InitializeComponent();
             _viewModel = new GestionnaireUserViewModel();
             DataContext = _viewModel; // Associer le ViewModel à la vue
+        }
+
+        private void Details_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is User user)
+            {
+                MessageBox.Show($"Détails de {user.Email}:\nRôle: {user.Role}", "Détails Utilisateur");
+            }
+        }
+
+        private void Modifier_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is User user)
+            {
+                MessageBox.Show($"Modifier l'utilisateur : {user.Email}", "Modification");
+            }
+        }
+
+        private void Supprimer_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is User user)
+            {
+                MessageBoxResult result = MessageBox.Show($"Supprimer {user.Email} ?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show($"Utilisateur {user.Email} supprimé.", "Suppression");
+                }
+            }
         }
     }
 }
